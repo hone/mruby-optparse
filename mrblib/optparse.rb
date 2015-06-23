e534c9313ad4e60ffc37f110374daf45fdbd2d4b
Terence Lee <hone02@gmail.com>
2015-06-23 14:57:48 -0400

    they're either the same or incompatible

--- mrblib/optparse.rb
+++ mrblib/optparse.rb
@@ -491,7 +491,7 @@ class OptionParser
       else
         t = Switch::RequiredArgument
       end
-      self >= t or incompatible_argument_styles(arg, t)
+      self == t or incompatible_argument_styles(arg, t)
       t
     end
 
